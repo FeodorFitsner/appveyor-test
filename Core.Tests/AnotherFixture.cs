10e0e862045413d3cfff6ac357aece925c0663b5
nulltoken <emeric.fermas@gmail.com>
2014-10-21 15:57:00 +0200

    More coverage

---
+++ Core.Tests/AnotherFixture.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Core.Tests
+{
+    public class AnotherFixture
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(3, Core.Random.Generate() - 1);
+        }
+    }
+}
